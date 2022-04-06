fb432fb2872deba30098771eaf428d983b52e830
Solevaras <100102064+Solevaras@users.noreply.github.com>
2022-04-07 00:31:16 +0700

    Add files via upload

---
+++ firstmodule.py
@@ -0,0 +1,33 @@
+import random
+from time import sleep
+from pyrogram import Client, filters, sync
+from pyrogram.errors import FloodWait
+import os
+from pyrogram import Client, filters
+from pyrogram.errors import FloodWait
+from pyrogram.types import ChatPermissions
+import time
+from .. import utils, loader
+   async def ilycmd(self, msg: "telethon.tl.types.Message") -> None:
+if not msg.out:
+            msg = await msg.respond("dina")
+def valentine(_, msg):
+   
+                msg.edit(f'❤️{i} ❤️')
+                sleep(time/cool)
+                msg.edit(f'🧡 {i} 🧡')
+                sleep(time/cool)
+                msg.edit(f'💛 {i} 💛')
+                sleep(time/cool)
+                msg.edit(f'💚 {i} 💚')
+                sleep(time/cool)
+                msg.edit(f'💙 {i} 💙')
+                sleep(time/cool)
+                msg.edit(f'💜 {i} 💜')
+                sleep(time/cool)
+                msg.edit(f'🖤 {i} 🖤')
+                sleep(time/cool)
+                msg.edit(f'🤍 {i} 🤍')
+                sleep(time/cool)
+            except:
+                pass
